d6049db2e230802c0997b6a077d6647a67791506
Witcher5566 <github77881@gmail.com>
2026-01-25 13:52:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+VbGKjUX2GuE0MDg9')  # 
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003542785911")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1002277927856")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
